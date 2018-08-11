a489d4e29ac1477c7bc6bebda4fda081eba9cfc3
Qmeng <qmeng0207@gmail.com>
2018-08-12 02:26:00 +0800

    dyn creat button for entry module

--- mine_sweeping/mode_choice.cpp
+++ mine_sweeping/mode_choice.cpp
@@ -3,6 +3,7 @@
 #include "ui_mode_choice.h"
 #include <iostream>
 
+
 using namespace std;
 
 mode_choice::mode_choice(QWidget *parent) :
@@ -10,6 +11,8 @@ mode_choice::mode_choice(QWidget *parent) :
     ui(new Ui::mode_choice)
 {
     ui->setupUi(this);
+    this->Init_modeWindow();
+
 }
 
 mode_choice::~mode_choice()
@@ -33,6 +36,63 @@ void mode_choice::on_modeButton_Normal_pressed()
     pNormalWindow->show();
     this->setVisible(false);
     connect(pNormalWindow,SIGNAL(close_signals()),this,SLOT(show()));
+}
+
+void mode_choice::on_modeButton_Designed_clicked()
+{
+    this->Set_modeButton_visible(false);
+    this->Set_designedButton_visible(true);
+}
+
+void mode_choice::on_designedButton_Return_clicked()
+{
+    this->Set_modeButton_visible(true);
+    this->Set_designedButton_visible(false);
+}
+
+void mode_choice::Set_modeButton_visible(bool flag)
+{
+    modeButton_normal->setVisible(flag);
+    modeButton_designed->setVisible(flag);
+    modeButton_Exit->setVisible(flag);
+}
+
+void mode_choice::Set_designedButton_visible(bool flag)
+{
+    designedButton_New->setVisible(flag);
+    designedButton_Load->setVisible(flag);
+    designedButton_Return->setVisible(flag);
+}
+
+void mode_choice::Init_modeWindow()
+{
+    this->modeButton_normal = new QPushButton("Normal");
+    this->modeButton_designed = new QPushButton("Designed");
+    this->modeButton_Exit = new QPushButton("Exit");
+    modeButton_normal->setSizePolicy(QSizePolicy::Policy::Preferred,QSizePolicy::Policy::Preferred);
+    modeButton_designed->setSizePolicy(QSizePolicy::Policy::Preferred,QSizePolicy::Policy::Preferred);
+    modeButton_Exit->setSizePolicy(QSizePolicy::Policy::Preferred,QSizePolicy::Policy::Preferred);
+    ui->verticalLayout->addWidget(modeButton_normal);
+    ui->verticalLayout->addWidget(modeButton_designed);
+    ui->verticalLayout->addWidget(modeButton_Exit);
+
+    this->designedButton_New = new QPushButton("New");
+    this->designedButton_Load = new QPushButton("Load");
+    this->designedButton_Return = new QPushButton("Return");
+    designedButton_New->setSizePolicy(QSizePolicy::Policy::Preferred,QSizePolicy::Policy::Preferred);
+    designedButton_Load->setSizePolicy(QSizePolicy::Policy::Preferred,QSizePolicy::Policy::Preferred);
+    designedButton_Return->setSizePolicy(QSizePolicy::Policy::Preferred,QSizePolicy::Policy::Preferred);
+    ui->verticalLayout->addWidget(designedButton_New);
+    ui->verticalLayout->addWidget(designedButton_Load);
+    ui->verticalLayout->addWidget(designedButton_Return);
+
+    connect(this->modeButton_normal,SIGNAL(clicked()),this,SLOT(on_modeButton_Normal_pressed()));
+    connect(this->modeButton_Exit,SIGNAL(clicked()),this,SLOT(on_modeButton_Exit_clicked()));
+    connect(this->modeButton_designed,SIGNAL(clicked()),this,SLOT(on_modeButton_Designed_clicked()));
+    connect(this->designedButton_Return,SIGNAL(clicked()),this,SLOT(on_designedButton_Return_clicked()));
+
+    this->Set_modeButton_visible(true);
+    this->Set_designedButton_visible(false);
 }
 
 
--- mine_sweeping/mode_choice.h
+++ mine_sweeping/mode_choice.h
@@ -4,6 +4,7 @@
 #include <QDialog>
 #include "mainwindow.h"
 #include <QCloseEvent>
+#include <QPushButton>
 
 namespace Ui {
 class mode_choice;
@@ -22,9 +23,25 @@ private slots:
 
     void on_modeButton_Normal_pressed();
 
+    void on_modeButton_Designed_clicked();
+
+    void on_designedButton_Return_clicked();
+
 private:
     Ui::mode_choice *ui;
     MainWindow *pNormalWindow;
+    QPushButton *modeButton_normal;
+    QPushButton *modeButton_designed;
+    QPushButton *modeButton_Exit;
+
+    QPushButton *designedButton_New;
+    QPushButton *designedButton_Load;
+    QPushButton *designedButton_Return;
+
+    void Set_modeButton_visible(bool flag);
+    void Set_designedButton_visible(bool flag);
+
+    void Init_modeWindow();
 
     void closeEvent(QCloseEvent *event);
 };
